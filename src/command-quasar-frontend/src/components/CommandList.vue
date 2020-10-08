4aadf491db9b9bda82090053703e00beb2b4ce90
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-08 16:16:09 +0100

    Command List in separated component

---
+++ src/command-quasar-frontend/src/components/CommandList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="q-ma-xl">
+      <q-scroll-area
+        :visible=true
+        style="height: 600px; max-width: 100%;"
+      >
+        <div v-for="command in commands" :key="command.id" class="q-py-md">
+          <q-card class="my-card text-black">
+              <q-card-section>
+              <div class="text-h6 text-blue">{{command.howTo}}</div>
+              <div class="text text-green">Platform: {{command.platform}}</div>
+              <div class="text text-right">
+              <code>{{command.commandLine}}</code>
+              </div>
+            </q-card-section>
+            <q-card-actions>
+              <q-btn flat>Edit</q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+      </q-scroll-area>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CommandList',
+  props: {
+    commands: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  .my-card
+    width: 100%
+    max-width: 70%
+</style>
--- src/command-quasar-frontend/src/pages/Index.vue
+++ src/command-quasar-frontend/src/pages/Index.vue
@@ -1,31 +1,14 @@
 <template>
-    <div class="q-ma-xl">
-      <q-scroll-area
-        :visible=true
-        style="height: 600px; max-width: 100%;"
-      >
-        <div v-for="command in commands" :key="command.id" class="q-py-md">
-          <q-card class="my-card text-black">
-              <q-card-section>
-              <div class="text-h6 text-blue">{{command.howTo}}</div>
-              <div class="text text-green">Platform: {{command.platform}}</div>
-              <div class="text text-right">
-              <code>{{command.commandLine}}</code>
-              </div>
-            </q-card-section>
-            <q-card-actions>
-              <q-btn flat>Edit</q-btn>
-            </q-card-actions>
-          </q-card>
-        </div>
-      </q-scroll-area>
-  </div>
+<command-list
+  :commands="commands"/>
 </template>
 
 <script>
+import CommandList from '../components/CommandList'
 import { fetchCommands } from '../actions'
+
 export default {
-  name: 'PageIndex',
+  components: { CommandList },
   data () {
     return {
       isDetailView: true,
@@ -39,9 +22,3 @@ export default {
   }
 }
 </script>
-
-<style lang="sass" scoped>
-  .my-card
-    width: 100%
-    max-width: 70%
-</style>
