82e087f89f458fdbc0cfa575b460b0e4da833556
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 21:05:03 +0100

    CommandListItem.vue Component created

--- src/command-quasar-frontend/src/components/CommandList.vue
+++ src/command-quasar-frontend/src/components/CommandList.vue
@@ -2,28 +2,9 @@
     <div class="q-ma-sm"
       v-if="isDetailView">
         <div v-for="command in commands" :key="command.id" class="q-py-md">
-          <q-card class=" text-white"
-                  style="background: linear-gradient(45deg, #486678, #343434)">
-              <q-card-section >
-              <div class="row">
-                <div class="col">
-                  <div class="text-h6 text-white q-ml-sm">{{command.howTo}}</div>
-                  <div class="text text-white q-ml-sm">{{command.platform}}</div>
-                  <div class="text text-green q-ml-sm q-mt-md">
-                    <code>{{command.commandLine}}</code>
-                  </div>
-                </div>
-                <div class="col-xl">
-                  <q-btn flat round dense
-                    @click="onEditButtonClicked(command)">
-                    <q-icon name="more_vert"></q-icon></q-btn></div>
-              </div>
-            </q-card-section>
-            <!-- <q-card-actions>
-              <q-btn type="update" color="primary"
-                @click="onEditButtonClicked(command)">Edit command</q-btn>
-            </q-card-actions> -->
-          </q-card>
+          <command-list-item
+            :command='command'
+            @on-edit-clicked="onEditButtonClicked"/>
         </div>
   </div>
   <command-edit
@@ -34,10 +15,12 @@
 </template>
 
 <script>
+import CommandListItem from './CommandListItem'
 import CommandEdit from './CommandEdit'
+
 export default {
   name: 'CommandList',
-  components: { CommandEdit },
+  components: { CommandEdit, CommandListItem },
   data () {
     return {
       selectedCommand: null,
---
+++ src/command-quasar-frontend/src/components/CommandListItem.vue
@@ -0,0 +1,44 @@
+<script>
+import CommandEdit from './CommandEdit'
+
+export default {
+  components: [CommandEdit],
+  props: {
+    command: {
+      type: Object
+    }
+  },
+  emits: ['on-edit-clicked'],
+  methods: {
+    onEditButtonClicked (selectedCommand) {
+      console.log('trying to emit')
+      this.$emit('on-edit-clicked', selectedCommand)
+    }
+  }
+}
+</script>
+
+<template>
+  <q-card class=" text-white"
+                    style="background: linear-gradient(45deg, #486678, #343434)">
+    <q-card-section >
+      <div class="row">
+        <div class="col">
+          <div class="text-h6 text-white q-ml-sm">{{command.howTo}}</div>
+          <div class="text text-white q-ml-sm">{{command.platform}}</div>
+          <div class="text text-green q-ml-sm q-mt-md">
+            <code>{{command.commandLine}}</code>
+          </div>
+        </div>
+        <div class="col-xl">
+          <q-btn flat round dense
+            @click="onEditButtonClicked(command)">
+            <q-icon name="more_vert"></q-icon></q-btn></div>
+      </div>
+    </q-card-section>
+  <!-- <q-card-actions>
+    <q-btn type="update" color="primary"
+      @click="onEditButtonClicked(command)">Edit command</q-btn>
+  </q-card-actions> -->
+  </q-card>
+</template>
